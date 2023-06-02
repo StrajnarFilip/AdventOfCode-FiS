6ed91083172a26ec62d408f677474d336738508c
Filip Strajnar <filip.strajnar@gmail.com>
2023-06-02 23:56:38 +0200

    Added Matrix class

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Fprog.Algorithms.Common.Structures;
+
+Matrix<int> x = new(new int[][]{
+    new int[]{ 1,2,3},
+    new int[]{ 1,5,3}
+});
+
+foreach (var item in x.GetRow(1))
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"{x.GetElement(0,1)}");
---
+++ Fprog.Algorithms.Common/Structures/Matrix.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fprog.Algorithms.Common.Structures
+{
+    public class Matrix<T>
+    {
+        readonly T[][] _matrix;
+
+        public Matrix(T[,] values)
+        {
+            if (values is null)
+            {
+                throw new ArgumentException("Null was given to constructor.");
+            }
+
+            this._matrix = values.Cast<T>().Chunk(values.GetLength(1)).ToArray();
+        }
+
+        public Matrix(IEnumerable<IEnumerable<T>> values)
+        {
+            this._matrix = values.Select(row => row.ToArray()).ToArray();
+        }
+
+        public int RowCount => this._matrix.Length;
+        public int ColumnsCount => this._matrix[0].Length;
+
+        public T[] GetRow(int index) => this._matrix[index];
+        public T[] GetColumn(int index) => this._matrix.Select(row => row[index]).ToArray();
+
+        public T? GetElement(int rowIndex, int columnIndex)
+        {
+            if (_matrix[rowIndex] is null || _matrix[rowIndex][columnIndex] is null)
+                return default;
+
+            return _matrix[rowIndex][columnIndex];
+        }
+
+        public T[] AllValues()
+        {
+            return this._matrix.SelectMany(row => row.ToArray()).ToArray();
+        }
+
+        public bool RowIndexInBounds(int rowIndex) => rowIndex < RowCount && rowIndex >= 0;
+        public bool ColumnIndexInBounds(int columnIndex) => columnIndex < ColumnsCount && columnIndex >= 0;
+        public bool IndicesInBounds(int rowIndex, int columnIndex)
+            => RowIndexInBounds(rowIndex) && ColumnIndexInBounds(columnIndex);
+
+        public (int row, int column)[] GetNeighbourIndices(int rowIndex, int columnIndex, bool includeDiagonal = false)
+        {
+            List<(int row, int column)> validNeighbourIndices = new();
+            // Left
+            if (IndicesInBounds(rowIndex, columnIndex - 1))
+                validNeighbourIndices.Add((rowIndex, columnIndex - 1));
+            // Right
+            if (IndicesInBounds(rowIndex, columnIndex + 1))
+                validNeighbourIndices.Add((rowIndex, columnIndex + 1));
+            // Up
+            if (IndicesInBounds(rowIndex - 1, columnIndex))
+                validNeighbourIndices.Add((rowIndex - 1, columnIndex));
+            // Down
+            if (IndicesInBounds(rowIndex + 1, columnIndex))
+                validNeighbourIndices.Add((rowIndex + 1, columnIndex));
+
+            if (includeDiagonal)
+            {
+                // Top Left
+                if (IndicesInBounds(rowIndex - 1, columnIndex - 1))
+                    validNeighbourIndices.Add((rowIndex - 1, columnIndex - 1));
+                // Top Right
+                if (IndicesInBounds(rowIndex - 1, columnIndex + 1))
+                    validNeighbourIndices.Add((rowIndex - 1, columnIndex + 1));
+                // Bottom Right
+                if (IndicesInBounds(rowIndex + 1, columnIndex + 1))
+                    validNeighbourIndices.Add((rowIndex + 1, columnIndex + 1));
+                // Bottom Left
+                if (IndicesInBounds(rowIndex + 1, columnIndex - 1))
+                    validNeighbourIndices.Add((rowIndex + 1, columnIndex - 1));
+            }
+
+            return validNeighbourIndices.ToArray();
+        }
+
+        public override string ToString()
+        {
+            return String.Join("\n", this._matrix.Select(row => String.Join(", ", row)));
+        }
+    }
+}
