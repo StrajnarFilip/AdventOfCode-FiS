37f663a45900bd991a6ff33c94303817e3978810
Filip Strajnar <filip.strajnar@gmail.com>
2023-06-03 13:59:20 +0200

    Merge sort beginning

---
+++ Fprog.Algorithms.Common/Sorting/MergeSortExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fprog.Algorithms.Common.Sorting
+{
+    public static class MergeSortExtension
+    {
+        public static IEnumerable<T> MergeSort<T>(this IEnumerable<T> collection) where T : IComparable<T>
+        {
+            if (collection is null)
+                return Array.Empty<T>();
+
+            int count = collection.Count();
+
+            if (count == 1)
+                return collection;
+
+
+            IEnumerable<T> firstPart = collection.Take(count / 2);
+            IEnumerable<T> lastPart = collection.Skip(count / 2);
+
+            var firstSolved = firstPart.MergeSort();
+            var lastSolved = lastPart.MergeSort();
+
+            // If first one is empty, return the last part.
+            if (!firstSolved.Any())
+                return lastSolved;
+            // If last part is empty, return the first part.
+            if (!lastSolved.Any())
+                return firstSolved;
+
+            // TODO
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Fprog.Algorithms.Common/Sorting/QuickSortExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fprog.Algorithms.Common.Sorting
+{
+    public static class QuickSortExtension
+    {
+        public static IEnumerable<T> QuickSort<T>(this IEnumerable<T> collection) where T : IComparable<T>
+        {
+            if (collection is null)
+                return Array.Empty<T>();
+
+            T[] values = collection.ToArray();
+
+            return values;
+        }
+    }
+}
