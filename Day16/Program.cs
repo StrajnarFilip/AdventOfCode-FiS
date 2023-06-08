8609e8581384d3ce1d008a2184844b5e7dced39f
Filip Strajnar <filip.strajnar@gmail.com>
2023-06-08 16:52:10 +0200

    Creating graph of valves

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -9,20 +9,36 @@ namespace Day16
         private static (string Valve, int Rate, string[] OutNeighbours) ExtractData(string line)
         {
             // Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
-            Regex rx = new Regex(
-                @"Valve ([A-Z]+) has flow rate=(0-9)+; tunnels lead to valves (.+)"
-            );
-            // string[] matched = rx.Matches(line);
-            // Console.WriteLine(matched.FormattedString());
+            string pattern = @"Valve (.+) has flow rate=(.+); tunnels* leads* to valves* (.+)";
+            Match match = Regex.Match(line, pattern);
 
-            return ("", 1, new string[] { });
+            string valveName = match.Groups[1].Value;
+            int rate = int.Parse(match.Groups[2].Value);
+            string[] outNeighbours = match.Groups[3].Value.Replace(" ", "").Split(",");
+
+            Console.WriteLine(
+                $"name: {valveName} rate: {rate} neighbours: {outNeighbours.FormattedString()}"
+            );
+
+            return (valveName, rate, outNeighbours);
         }
 
         static void Main(string[] args)
         {
-            var lines = File.ReadAllLines("Assets/data.txt");
+            var lines = File.ReadAllLines("Assets/test.txt");
             var lineDetails = lines.Select(ExtractData).ToArray();
-            Graph<Valve> graph = new Graph<Valve>();
+            Graph<Valve> graph = new Graph<Valve>(
+                lineDetails.Select(valve => new Valve(valve.Rate, valve.Valve))
+            );
+            foreach (var valve in lineDetails)
+            {
+                Valve currentValve = graph.Vertices.Single(el => el.Name == valve.Valve);
+                foreach (var outNeighbour in valve.OutNeighbours)
+                {
+                    Valve currentNeighbour = graph.Vertices.Single(el => el.Name == outNeighbour);
+                    graph.AddEdge(new Edge<Valve>(currentValve, currentNeighbour, 1));
+                }
+            }
         }
     }
 }
