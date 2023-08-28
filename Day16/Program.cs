906c590288cf7ecb8073c85ca22c8f1997488dad
Filip Strajnar <filip.strajnar@gmail.com>
2023-08-28 21:27:23 +0200

    More refactoring, removed old method.

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
 using Fprog.Algorithms.Common;
 using Fprog.Algorithms.Common.Structures;
 
@@ -28,11 +29,21 @@ namespace Day16
 
         private static int BestOutcomeClose(
             Graph<Valve> graph,
+            List<Valve> visited,
             TraversingEntity me,
             Dictionary<(Valve starting, Valve ending), List<Edge<Valve>>> quickestPaths,
             int previousStepsReleased
         )
         {
+            // If there is either no time left, all valves with any flow are visited
+            // or the current node was already visited, simply return the result.
+            if (
+                me.OutOfTime()
+                || graph.Vertices.Where(vertex => vertex.FlowRate > 0).All(visited.Contains)
+                || visited.Contains(me.CurrentValve)
+            )
+                return previousStepsReleased;
+
             IEnumerable<Valve> valvesToVisit = graph.Vertices.Where(
                 vertex => vertex.FlowRate > 0 && !vertex.Equals(me.CurrentValve)
             );
@@ -40,10 +51,10 @@ namespace Day16
             return valvesToVisit
                 .Select(
                     valve =>
-                        ValveOpeningRecursive(
-                            me.MakeMoves(quickestPaths[(me.CurrentValve, valve)]),
+                        BestOutcomeOpen(
                             graph,
-                            new List<Valve>(),
+                            visited,
+                            me.MakeMoves(quickestPaths[(me.CurrentValve, valve)]),
                             quickestPaths,
                             previousStepsReleased
                         )
@@ -53,23 +64,33 @@ namespace Day16
 
         private static int BestOutcomeOpen(
             Graph<Valve> graph,
+            List<Valve> visited,
             TraversingEntity me,
             Dictionary<(Valve starting, Valve ending), List<Edge<Valve>>> quickestPaths,
             int previousStepsReleased
         )
         {
+            // If there is either no time left, all valves with any flow are visited
+            // or the current node was already visited, simply return the result.
+            if (
+                me.OutOfTime()
+                || graph.Vertices.Where(vertex => vertex.FlowRate > 0).All(visited.Contains)
+                || visited.Contains(me.CurrentValve)
+            )
+                return previousStepsReleased;
+
             IEnumerable<Valve> valvesToVisit = graph.Vertices.Where(
                 vertex => vertex.FlowRate > 0 && !vertex.Equals(me.CurrentValve)
             );
-            var released = me.CurrentValve.FlowRate * (startingMinutes - 1);
+            var released = me.CurrentValve.FlowRate * (me.TimeLeft - 1);
 
             return valvesToVisit
                 .Select(
                     valve =>
-                        ValveOpeningRecursive(
-                            me.OpenAndMakeMoves(quickestPaths[(me.CurrentValve, valve)]),
+                        BestOutcomeOpen(
                             graph,
-                            new List<Valve>() { me.CurrentValve },
+                            visited.Append(me.CurrentValve).ToList(),
+                            me.OpenAndMakeMoves(quickestPaths[(me.CurrentValve, valve)]),
                             quickestPaths,
                             previousStepsReleased + released
                         )
@@ -84,59 +105,12 @@ namespace Day16
 
             var startingValve = graph.Vertices.Single(valve => valve.Name == "AA");
             var me = new TraversingEntity("Me", startingMinutes, startingValve);
+            var visited = new List<Valve>();
 
-            int outcomesLeftClosed = BestOutcomeClose(graph, me, quickestPaths, 0);
-            int outcomesOpened = BestOutcomeOpen(graph, me, quickestPaths, 0);
+            int outcomesLeftClosed = BestOutcomeClose(graph, visited, me, quickestPaths, 0);
+            int outcomesOpened = BestOutcomeOpen(graph, visited, me, quickestPaths, 0);
 
             return Math.Max(outcomesLeftClosed, outcomesOpened);
-        }
-
-        private static int ValveOpeningRecursive(
-            TraversingEntity me,
-            Graph<Valve> graph,
-            List<Valve> visited,
-            Dictionary<(Valve starting, Valve ending), List<Edge<Valve>>> quickestPaths,
-            int previousStepsReleased
-        )
-        {
-            // If there is either no time left, all valves with any flow are visited
-            // or the current node was already visited, simply return the result.
-            if (
-                me.OutOfTime()
-                || graph.Vertices.Where(vertex => vertex.FlowRate > 0).All(visited.Contains)
-                || visited.Contains(me.CurrentValve)
-            )
-                return previousStepsReleased;
-
-            List<Valve> newVisited = visited.Append(me.CurrentValve).ToList();
-
-            // Valves we have yet to visit must have positive flow rate.
-            var valvesToVisit = graph.Vertices.Where(
-                vertex => vertex.FlowRate > 0 && !vertex.Equals(me.CurrentValve)
-            );
-
-            // Pressure released, if we decide to open.
-            // It takes one minute until it's open, hence - 1.
-            var openingRelased = me.CurrentValve.FlowRate * (me.TimeLeft - 1);
-            // Open the valve
-            int[] outcomesIfOpened = valvesToVisit
-                .Select(
-                    valve =>
-                        ValveOpeningRecursive(
-                            me.OpenAndMakeMoves(quickestPaths[(me.CurrentValve, valve)]),
-                            graph,
-                            newVisited,
-                            quickestPaths,
-                            previousStepsReleased + openingRelased
-                        )
-                )
-                .ToArray();
-
-            int bestOutcomeIfOpened = outcomesIfOpened.Any()
-                ? outcomesIfOpened.Max()
-                : previousStepsReleased + openingRelased;
-
-            return bestOutcomeIfOpened;
         }
 
         private static int Part1(Graph<Valve> graph)
